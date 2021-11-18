bea9039754c328c42268c5c05bbe22e77a540539
Oleksii Ulianov <alexallorayz@gmail.com>
2021-11-18 11:33:51 +0200

    17.11.2021_Prime numbers

---
+++ liveCoding/17.11.2021_Prime numbers/index.js
@@ -0,0 +1,23 @@
+// How to work on tech tasks. Step by step guide
+
+// 1. Learn requirement  +++
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
+
+// algo
+// 1. iterate nembers from 2 to num => i
+// 2. iterate from 2 to i => number
+// 3. if % === 0(остаток от деления 0) - not prime(не простое)// если остаток от деление i на number равно 0 - то не простое
+// 4.
+
+/**
+ * @param {number} num
+ * @return {undefined}
+ */
+
+// input: number
+// output: undefined
+function getPrimes(num) {
+  // put your code here
+}
