4db855fd8aa0cbb26f40faef73a3b9468262160c
Oleksii Ulianov <alexallorayz@gmail.com>
2021-11-18 11:33:22 +0200

    config-for-JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
-import { rules } from 'eslint-config-prettier';
-
 module.exports = {
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    sourceType: 'module',
-    allowImportExportEverywhere: false,
-    ecmaFeatures: {
-      globalReturn: false,
-    },
-    babelOptions: {
-      configFile: 'path/to/config.js',
-    },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  env: {
+    es6: true,
+    jest: true,
+    browser: true,
   },
-};
-export const rules = {
-  'no-console': 0,
-  'import/prefer-default-export': 0,
-  'prefer-template': 0,
+  rules: {
+    strict: 0,
+    'no-alert': 0,
+    'no-console': 0,
+    'no-constant-condition': 0,
+    'no-shadow': 0,
+    'no-restricted-globals': 0,
+    'no-unused-vars': 0,
+    'no-underscore-dangle': 0,
+    'func-names': 0,
+    'global-require': 0,
+    'import/extensions': 0,
+    'import/no-dynamic-require': 0,
+    'import/prefer-default-export': 0,
+    'prefer-template': 0,
+  },
 };
