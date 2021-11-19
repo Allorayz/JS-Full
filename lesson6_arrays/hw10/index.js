3b453a33d4edd79d1507a7b569c7fbf169df490f
Oleksii Ulianov <alexallorayz@gmail.com>
2021-11-19 20:36:04 +0200

    lesson6_Первые n элементов исходного массива

---
+++ lesson6_arrays/hw10/index.js
@@ -0,0 +1,8 @@
+const getSubArray = (arr, numberOfElements) => {
+  return arr.slice(0, numberOfElements);
+};
+
+console.log(getSubArray([11, 4, 8, 3], 2));
+console.log(getSubArray([11, 4, 8, 3], 2)); // ==> [11, 4]
+console.log(getSubArray([1, 2, 3, 4, 5], 3)); // ==> [1, 2, 3]
+console.log(getSubArray(['some string', 'hello', 'I am happy'], 1)); // ==> ['some string']
