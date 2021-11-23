5cfed991741947ba34181fa1b19af407331b75da
Oleksii Ulianov <alexallorayz@gmail.com>
2021-11-23 23:46:54 +0200

    lesson10_Проверка чисел

---
+++ lesson10/task1/index.js
@@ -0,0 +1,17 @@
+const array = ['17', '17.5', 17, 17, 5, NaN, undefined, null];
+
+const getFiniteNumbers = arr => arr.filter(el => Number.isFinite(el));
+
+console.log(getFiniteNumbers(array));
+
+const getFiniteNumbersV2 = arr => arr.filter(el => isFinite(el));
+console.log(getFiniteNumbersV2(array));
+
+const getNaN = arr => arr.filter(el => Number.isNaN(el));
+console.log(getNaN(array));
+
+const getNaNV2 = arr => arr.filter(el => isNaN(el));
+console.log(getNaNV2(array));
+
+const getInteges = arr => arr.filter(el => Number.isInteger(el));
+console.log(getInteges(array));
