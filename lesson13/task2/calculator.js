67f2be873f7b846c41a89a9c55a4b933f8a28f66
Oleksii Ulianov <alexallorayz@gmail.com>
2021-11-29 00:09:30 +0200

    lesson13_Test code in different files

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -1,7 +1,5 @@
-export const getSquareNumbers = arr => arr.map(num => num * num);
+export const getSquaredArray = arr => arr.map(num => num * num);
 
 export const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
 
 export default (a, b) => a + b;
-
-console.log(getSquareNumbers([1, 2, 3]));
