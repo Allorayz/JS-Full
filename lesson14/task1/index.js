7c50c780ce9bb9cacd9a3e1eadc50f0f8f776261
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-01 09:38:50 +0200

    lesson14_Лексическое окружение

---
+++ lesson14/task1/index.js
@@ -0,0 +1,15 @@
+let message = 'Just learn it';
+// console.log(message);
+
+function sendMessage(name) {
+  const sender = 'Gromcode';
+  console.log(`${name}, ${message}! Your ${sender} `);
+}
+
+function setMessage(text) {
+  message = text;
+}
+
+// sendMessage('Ann');
+// setMessage('Good job');
+// sendMessage('Ann');
