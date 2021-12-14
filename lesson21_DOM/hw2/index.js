1a658e475bfaa80d044989cee726fc05495d2df1
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 00:13:05 +0200

    lesson21_Поиск нескольких элементов на странице

--- lesson21_DOM/hw2/index.js
+++ lesson21_DOM/hw2/index.js
@@ -1,14 +1,15 @@
-function getItemList() {
+export function getItemsList() {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 }
 
-function getItemsArray() {
-  const elementArray = Array.from(document.querySelectorAll('.tool'));
-  console.dir(elementArray);
-  return elementArray;
-}
-getItemList();
+getItemsList();
 
-export { getItemList, getItemsArray };
+export function getItemsArray() {
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.dir(elementsArray);
+  return elementsArray;
+}
+
+getItemsArray();
