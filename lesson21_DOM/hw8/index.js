788a4b493246065eae0f14ac58474b5b20c2cfb2
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 01:37:16 +0200

    lesson21_Создание DOM элементов и вставка на страницу - 2

---
+++ lesson21_DOM/hw8/index.js
@@ -0,0 +1,21 @@
+function finishList() {
+  const itemOne = document.createElement('li');
+  itemOne.textContent = '1';
+  document.querySelector('ul').prepend(itemOne);
+
+  const itemFour = document.createElement('li');
+  itemFour.textContent = '4';
+  document.querySelector('.special').before(itemFour);
+
+  const itemSix = document.createElement('li');
+  itemSix.textContent = '6';
+  document.querySelector('.special').after(itemSix);
+
+  const itemEight = document.createElement('li');
+  itemEight.textContent = '8';
+  document.querySelector('ul').append(itemEight);
+}
+
+// finishList();
+
+export { finishList };
