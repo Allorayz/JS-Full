10c821d299d76ebd375a49035f512366da8a272f
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-14 19:33:05 +0200

    lesson20_Защищенные поля

---
+++ lesson20_classes/task3/index.js
@@ -0,0 +1,30 @@
+class Wallet {
+  balance = 0;
+
+  getBalance() {
+    return this.balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      console.log('No enough funds');
+      return;
+    }
+    this.balance -= amount;
+  }
+}
+
+const walletUser = new Wallet();
+
+// console.log(walletUser.getBalance());
+// walletUser.deposit(100);
+// console.log(walletUser.getBalance());
+// walletUser.withDraw(45);
+// walletUser.withDraw(100);
+// console.log(walletUser.getBalance());
+
+export { Wallet };
