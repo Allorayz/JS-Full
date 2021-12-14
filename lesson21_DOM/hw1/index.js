db0a695b5d8b957855cf1a90a4bffc51e24ff932
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-14 23:09:49 +0200

    lesson21_Поиск элемента на странице

--- lesson21_DOM/hw1/index.js
+++ lesson21_DOM/hw1/index.js
@@ -1,5 +1,5 @@
 function getTitleElement() {
-  const titleElem = document.querySelector('title');
+  const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 }
