c18654dcff617740dd09430c947443588ae3e1dc
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-14 23:08:13 +0200

    lesson21_Поиск элемента на странице

---
+++ lesson21_DOM/hw1/index.js
@@ -0,0 +1,19 @@
+function getTitleElement() {
+  const titleElem = document.querySelector('title');
+  console.dir(titleElem);
+  return titleElem;
+}
+
+// test data1
+getTitleElement();
+
+function getInputElement() {
+  const inputElem = document.querySelector('[type = "text"]');
+  console.dir(inputElem);
+  return inputElem;
+}
+
+// test data 2
+getInputElement();
+
+export { getTitleElement, getInputElement };
