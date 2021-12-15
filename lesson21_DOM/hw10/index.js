f1d4bae348b551ec27d8ce818fcb5b6233e3ddf9
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 02:04:28 +0200

    lesson21_Работа с классами для DOM элементов

---
+++ lesson21_DOM/hw10/index.js
@@ -0,0 +1,19 @@
+function manageClasses() {
+  const firstListItem = document.querySelector('.one');
+  firstListItem.classList.add('selected');
+
+  const secondListItem = document.querySelector('.two');
+  secondListItem.classList.remove('selected');
+
+  const thirdListItem = document.querySelector('.three');
+  thirdListItem.classList.toggle('three_done');
+
+  const fourListItem = document.querySelector('.four');
+  if (fourListItem.classList.contains('some-class')) {
+    fourListItem.classList.add('another-class');
+  }
+}
+
+// manageClasses();
+
+export { manageClasses };
