f15a95c6ee0aa409bb61376014c63db011a8ae3b
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 11:38:15 +0200

    liveCoding 15.12.2021

---
+++ liveCoding/15.12.2021/index.js
@@ -0,0 +1,80 @@
+const listElem = document.querySelector('.list');
+
+const tasks = [
+  { id: 1, text: 'Buy milk', done: false },
+  { id: 2, text: 'Pick up Tom from airport', done: false },
+  { id: 3, text: 'Visit party', done: false },
+  { id: 4, text: 'Visit doctor', done: true },
+  { id: 5, text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  listElem.innerHTML = '';
+
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }, index) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.setAttribute('data-id', index);
+      checkbox.checked = done;
+
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+renderTasks(tasks);
+
+// EVENTS
+// 1. add event to the element
+// 2. create event handlers
+
+// list
+// click
+
+// input: event
+// output: undef
+
+// algo
+// 0. get task id
+// 1.find task by id
+// 2. update task
+// re-render
+
+function updateTaskHandler(event) {
+  console.dir(event.target.classList.contains('list__item-checkbox'));
+  console.dir(event.target.getAttribute('type'));
+
+  if (!event.target.classList.contains('list__item-checkbox')) {
+    return;
+  }
+
+  //   const id = event.target.dataset.id;
+  const { id } = event.target.dataset;
+
+  // TODO
+
+  // доделать этот метод и создать новый метод для добавления новой строки
+}
+
+// inp string(event) func
+// out undefined
+const res = listElem.addEventListener('click', updateTaskHandler);
+
+// WEB FLOW
+// 1. get data
+// 2. render
+// 3. update daya, don't update DOM (!!!!)
+// 4. re-render
