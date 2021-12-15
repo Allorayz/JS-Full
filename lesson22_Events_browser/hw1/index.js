5ae04c77155c5f3f69fb82564e2b8414dce53ebe
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 09:40:41 +0200

    lesson22_Обработка события 'click'

--- lesson22_Events_browser/hw1/index.js
+++ lesson22_Events_browser/hw1/index.js
@@ -1,3 +1,4 @@
 const createBtn = document.querySelector('.create-btn');
-const done = () => alert('done');
-createBtn.addEventListener('click', done);
+createBtn.addEventListener('click', () => {
+  alert('done');
+});
