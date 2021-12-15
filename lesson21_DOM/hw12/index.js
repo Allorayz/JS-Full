37481d5658e47976f02d2cb0c9ae00aca685a175
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-15 02:50:12 +0200

    lesson21_Нахождение ближайшего родителя

---
+++ lesson21_DOM/hw12/index.js
@@ -0,0 +1,9 @@
+function getSection(num) {
+  const span = document.querySelector(`span[data-number="${num}"]`);
+  //   console.log(span);
+  return span.closest('.box').getAttribute('data-section');
+}
+
+// getSection(6);
+
+export { getSection };
