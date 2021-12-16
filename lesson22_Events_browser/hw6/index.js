b330c52270b56d0ec5f32ffe4b7c95bcfdaeba45
Oleksii Ulianov <alexallorayz@gmail.com>
2021-12-16 08:52:21 +0200

    lesson22_Event target - считывание текста с кнопки

--- lesson22_Events_browser/hw6/index.js
+++ lesson22_Events_browser/hw6/index.js
@@ -1,8 +1,9 @@
 const btn = document.querySelectorAll('.btn');
-console.log(btn);
+// console.log(btn);
 function handleClick(event) {
   console.log(event.target.textContent);
 }
 
-btn[0].addEventListener('click', handleClick);
-btn[1].addEventListener('click', handleClick);
+btn.forEach(el => el.addEventListener('click', handleClick));
+// btn[0].addEventListener('click', handleClick);
+// btn[1].addEventListener('click', handleClick);
