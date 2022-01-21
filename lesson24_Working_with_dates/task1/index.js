9ea0337db6d21f6b4ac48e49125a0439ee20f9cb
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-01-21 19:39:23 +0200

    Установка компонентов даты

---
+++ lesson24_Working_with_dates/task1/index.js
@@ -0,0 +1,10 @@
+const dayOfWeek = (date, days) => {
+  const weekDays = ['Mo', 'Tu', 'We', 'Th', 'St', 'Su'];
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = dayOfWeek(new Date(2019, 0, 1), 14);
+console.log(result);
