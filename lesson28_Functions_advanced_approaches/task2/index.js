e047c6245c23f2df879c34072a08a9b60be165d4
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-01-27 13:32:54 +0200

    Проход по дереву с помощью рекурсии

---
+++ lesson28_Functions_advanced_approaches/task2/index.js
@@ -0,0 +1,27 @@
+const favorites = ['id-2'];
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [],
+    },
+  ],
+};
+
+const markFavorites = (tree, favorites) => {
+  const isFavotite = favorites.includes(tree.id);
+
+  return {
+    ...tree,
+    isFavotite,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
+
+const result = markFavorites(tree, favorites);
+console.log(result);
+
+export { markFavorites };
