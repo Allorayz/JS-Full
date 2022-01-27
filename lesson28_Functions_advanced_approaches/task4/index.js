0f15c6b704f12ddf6b0a47c03736efc6dbee5859
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-01-27 14:54:13 +0200

    Композиция

---
+++ lesson28_Functions_advanced_approaches/task4/index.js
@@ -0,0 +1,12 @@
+const add5 = value => value + 5; //
+const mult3 = value => value * 3; //
+const div4 = value => value / 4; // набор коротких функций которые будут входить в композицю
+
+const compose =
+  (...funcs) =>
+  value => {
+    return funcs.reduce((acc, func) => func(acc), value);
+  }; // композиция, которая через каррирование принимает первым аргументом набор функций, а вторым аргументов значение (value)
+
+const doEverything = compose(add5, mult3, div4); // поочередное выполнениие функций
+console.log(doEverything(12));
