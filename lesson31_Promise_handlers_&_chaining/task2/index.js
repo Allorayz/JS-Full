92a4ab43483e1ad7f11c988eef4d1b26af1db3dd
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-02-14 19:53:46 +0200

    Цепочка промисов

--- lesson31_Promise_handlers_&_chaining/task2/index.js
+++ lesson31_Promise_handlers_&_chaining/task2/index.js
@@ -1,26 +1,24 @@
-export const asyncCalculator = numbr =>
+const asyncCalculator = numbr =>
   new Promise(resolve => {
     setTimeout(() => {
       console.log(`Initial value: ${numbr}`);
       resolve(numbr);
     }, 500);
-  }).then(
-    value =>
-      new Promise(resolve => {
-        setTimeout(() => {
-          const result = value * value;
-          console.log(`Squared value: ${result}`);
-          resolve(result);
-        }, 500);
-      }),
-  );
+  })
+    .then(
+      value =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+          }, 500);
+        }),
+    )
+    .then(value => {
+      const result = value * 2;
+      console.log(`Doubled value: ${result}`);
+      return result;
+    });
 
-const p = asyncCalculator(5);
-
-p.then(value => console.log(value));
-
-p.then(value => {
-  const result = value * 2;
-  console.log(`Doubled value: ${result}`);
-  return result;
-});
+asyncCalculator(5).then(result => console.log(result));
