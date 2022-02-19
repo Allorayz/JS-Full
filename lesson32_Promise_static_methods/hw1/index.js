fa06c329bc410d30c8e834b026d8bdf6d110ac1f
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-02-19 14:33:50 +0200

    Преобразователь в промис

---
+++ lesson32_Promise_static_methods/hw1/index.js
@@ -0,0 +1,14 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+export const makePromise = number => {
+  return Promise.resolve(number);
+};
+
+/*
+ * пример использования
+ */
+makePromise(17).then(number => {
+  console.log(number); // 17
+});
