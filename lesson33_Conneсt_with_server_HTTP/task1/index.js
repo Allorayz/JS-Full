237f3aff973a0a961ca1f53dffc950f178a2b23f
Oleksii Ulianov <92323362+Allorayz@users.noreply.github.com>
2022-02-24 09:28:05 +0200

    GET request with fetch

--- lesson33_Conneсt_with_server_HTTP/task1/index.js
+++ lesson33_Conneсt_with_server_HTTP/task1/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
@@ -6,33 +7,24 @@ const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 
 userAvatarElem.src = defaultAvatar;
 
-// https://api.github.com/users/USERNAME
-
-// fetch and render user data ...
-
-const fetchUserData = async userName => {
-  const response = await fetch(`https://api.github.com/users/${userName}`);
-  return await response.json();
-};
-
-const renderUserData = userData => {
-  // console.log(userData);
-  const { avatar_url, name, location } = userData;
-  userAvatarElem.src = avatar_url;
-  userNameElem.textContent = name;
-  userLocationElem.textContent = location
-    ? `from ${location}`
-    : '';
-};
-
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 
+const fetchUserData = (userName) =>
+  fetch(`https://api.github.com/users/${userName}`).then((response) =>
+    response.json()
+  );
+
+const renderUserData = (userData) => {
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location ? `from ${location}` : '';
+};
+
 const onSearchUser = () => {
   const userName = userNameInputElem.value;
-  fetchUserData(userName)
-    .then(userData => renderUserData(userData));
-
+  fetchUserData(userName).then((userData) => renderUserData(userData));
 };
 
 showUserBtnElem.addEventListener('click', onSearchUser);
