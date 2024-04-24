a52d257185bf25c3dba6bcf298e3cc1923785e33
Oleksii Ulianov <alexallorayz@gmail.com>
2024-04-24 21:41:58 +0300

    Update index.js

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -33,38 +33,3 @@ function updateUser(userId, userData) {
 		body: JSON.stringify(userData),
 	});
 }
-
-// examples
-getUsersList().then(users => {
-	console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
-
-getUserById('5').then(userData => {
-	console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
-
-const newUserData = {
-	email: 'cool@email.com',
-	firstName: 'Iron',
-	lastName: 'Man',
-	age: 42,
-};
-
-createUser(newUserData).then(() => {
-	console.log('User created');
-});
-
-const updatedUserData = {
-	email: 'new@email.com',
-	firstName: 'John',
-	lastName: 'Doe',
-	age: 17,
-};
-
-updateUser('1', updatedUserData).then(() => {
-	console.log('User updated');
-});
-
-deleteUser('2').then(() => {
-	console.log('User updated');
-});
