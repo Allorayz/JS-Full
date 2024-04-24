5d9dce2a756266c56428075c4cd62bf73363b098
Oleksii Ulianov <alexallorayz@gmail.com>
2024-04-24 23:01:41 +0300

    Update index.js

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -1,46 +1,70 @@
-const emailInputElem = document.getElementById('email');
-const passwordInputElem = document.querySelector('[name="name"]');
-const userNameElem = document.querySelector('[name="password"]');
-const registerBtn = document.querySelector('.submit-button');
-const formElement = document.querySelector('.login-form');
+const baseUrl = 'https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users';
 
-const baseUrl = 'https://66292e6c54afcabd07387955.mockapi.io/api/v1/users';
+export function getUsersList() {
+	return fetch(baseUrl).then(resolve => resolve.json());
+}
 
-export const isValid = () => {
-	if (
-		emailInputElem.reportValidity() &&
-		userNameElem.reportValidity() &&
-		passwordInputElem.reportValidity()
-	) {
-		registerBtn.disabled = false;
-	}
-};
+export function getUserById(userId) {
+	return fetch(`${baseUrl}/${userId}`).then(resolve => resolve.json());
+}
 
-export const sendForm = formData => {
+export function createUser(userData) {
 	return fetch(baseUrl, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify(formData),
+		body: JSON.stringify(userData),
 	});
+}
+
+export function deleteUser(userId) {
+	return fetch(`${baseUrl}/${userId}`, {
+		method: 'DELETE',
+	});
+}
+
+export function updateUser(userId, userData) {
+	return fetch(`${baseUrl}/${userId}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(userData),
+	});
+}
+
+// examples
+getUsersList().then(users => {
+	console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+});
+
+getUserById('5').then(userData => {
+	console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
+
+const newUserData = {
+	email: 'cool@email.com',
+	firstName: 'Iron',
+	lastName: 'Man',
+	age: 42,
 };
 
-export const onFormSubmit = event => {
-	event.preventDefault();
-	const formData = [...new FormData(formElement)].reduce(
-		(acc, [field, value]) => ({ ...acc, [field]: value }),
-		{}
-	);
+createUser(newUserData).then(() => {
+	console.log('User created');
+});
 
-	sendForm(formData)
-		.then(response => response.json())
-		.then(userData => {
-			alert(JSON.stringify(userData));
-			formElement.reset();
-			registerBtn.disabled = true;
-		});
+const updatedUserData = {
+	email: 'new@email.com',
+	firstName: 'John',
+	lastName: 'Doe',
+	age: 17,
 };
 
-formElement.addEventListener('submit', onFormSubmit);
-formElement.addEventListener('input', isValid);
+updateUser('1', updatedUserData).then(() => {
+	console.log('User updated');
+});
+
+deleteUser('2').then(() => {
+	console.log('User updated');
+});
