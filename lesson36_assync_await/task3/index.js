4625da5b0fdf6b65358d565b9471e092ca9222d1
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-11 12:51:35 +0300

    Users blogs

--- lesson36_assync_await/task3/index.js
+++ lesson36_assync_await/task3/index.js
@@ -4,23 +4,34 @@
  * @param {string[]} users
  * @return {promise}
  */
-
-export const getUsersBlogs = async ([...users]) => {
-	const res = await Promise.all(
-		users.map(async (user) => {
-			const response = await fetch(
-				`https://api.github.com/users/${user}`
-			);
-			const userData = await response.json();
-			const { blog } = userData;
-			return blog;
-		}, [])
-	);
-	return res;
+// my finale
+export const getUsersBlogs = ([...users]) => {
+	const res = users.map(async (user) => {
+		const response = await fetch(`https://api.github.com/users/${user}`);
+		const userData = await response.json();
+		const { blog } = userData;
+		return blog;
+	});
+	return Promise.all(res);
 };
 
-// examples
-// getUsersBlogs(["google", "facebook", "reactjs"]).then((linksList) =>
-// 	console.log(linksList)
-// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-// getUsersBlogs(["microsoft"]).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// first variant
+// export const getUsersBlogs = async ([...users]) => {
+// 	const res = await Promise.all(
+// 		users.map(async (user) => {
+// 			const response = await fetch(
+// 				`https://api.github.com/users/${user}`
+// 			);
+// 			const userData = await response.json();
+// 			const { blog } = userData;
+// 			return blog;
+// 		}, [])
+// 	);
+// 	return res;
+// };
+
+// examples;
+getUsersBlogs(["google", "facebook", "reactjs"]).then((linksList) =>
+	console.log(linksList)
+); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+getUsersBlogs(["microsoft"]).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
