9e06209cc4a66595164a8898b84f67ccdda3e211
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-11 12:41:34 +0300

    Users blogs

---
+++ lesson36_assync_await/task3/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * @param {string[]} users
+ * @return {promise}
+ */
+
+export const getUsersBlogs = async ([...users]) => {
+	const res = await Promise.all(
+		users.map(async (user) => {
+			const response = await fetch(
+				`https://api.github.com/users/${user}`
+			);
+			const userData = await response.json();
+			const { blog } = userData;
+			return blog;
+		}, [])
+	);
+	return res;
+};
+
+// examples
+getUsersBlogs(["google", "facebook", "reactjs"]).then((linksList) =>
+	console.log(linksList)
+); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+getUsersBlogs(["microsoft"]).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
